29ea1428798ff1509098f0e19093d0f76e2b9ae7
Jubaw <doguztrk@gmail.com>
2024-11-07 11:06:47 +0300

    fst

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+	return (
+		<main className="">
+			<h1>Lets build an API productivity APP</h1>
+      
+			<Button>Click me</Button>
+		</main>
+	);
+}
